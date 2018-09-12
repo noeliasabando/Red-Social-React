bbed46c485d0c88ac7b725a441f9cc03122f3a3a
Noelia Sabando <lolia_15@hotmail.com>
2018-09-12 16:57:44 -0400

    componente logo + router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
-import Perfil from "./components/Perfil/Perfil"
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+
+import Inicio from "./components/Inicio/Inicio";
+import Perfil from "./components/Perfil/Perfil";
+import Amigos from "./components/Amigos/Amigos"; 
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="Perfil">
-       <Perfil />
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Inicio} />    
+          <Route path="/profile" component={Perfil} />     
+          <Route path="/friends" component={Amigos} />       
+        </div>          
+      </Router>
     );
   }
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { NavbarToggler} from 'reactstrap';
+import { NavLink, withRouter}  from "react-router-dom";
+
+import "./Navbar.css";
+
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div >
+        <header>
+          <nav className="navbar navbar-expand-lg">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon">
+                <i className="fas fa-bars"></i>
+              </span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-4">
+                <li className="nav-item mr-3 active">
+                  <NavLink className="nav-link" to='/profile' activeclassname="active">Ver perfil</NavLink>
+                </li>
+                <li className="nav-item mr-3">
+                  <NavLink className="nav-link" to='/'>Inicio</NavLink>
+                </li>
+                <li className="nav-item mr-3">
+                  <NavLink className="nav-link" to='/friends' >Amigos</NavLink>
+                </li>
+                <li className="nav-item mr-3">
+                  <NavLink className="nav-link" to='/login' >Cerrar sesión</NavLink>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default withRouter(Navbar);
+
+
+
+
+
+
+
+
