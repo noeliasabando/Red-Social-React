bffe668487357638ae8515cf3dab0c7c9a59c5d2
Sabando Gonzalez <lolia_15@hotmail.com>
2018-09-20 03:09:02 -0400

    Revert "ruta"
    This reverts commit c6d9051b18e8f4c60ed7700baba5ad1553be55a5.

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ const App = () =>
 class App extends Component {
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <div>
           <Route exact path="/" component={Register} />    
           <Route path="/login" component={Login} />    
