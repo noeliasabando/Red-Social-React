5f4fb3736400ca8583511d9ec4ae60c04525c78a
Noelia Sabando <lolia_15@hotmail.com>
2018-09-20 01:02:19 -0400

    imprime nombre y mail en perfil

--- src/components/InfoPerfil/InfoPerfil.jsx
+++ src/components/InfoPerfil/InfoPerfil.jsx
@@ -1,18 +1,20 @@
 import React, {Component} from 'react';
 import './InfoPerfil.css';
 import UserImg from "../../img/usuaria2.png"
+import Auth from '../../Firebase/auth';
 
 
 class InfoPerfil extends Component{
   render(){
+    const user= Auth.currentUser;
     return(
       <div className="contenedorPerfil">
         <div className="col-12 text-center">
           <img className="userImg" src= {UserImg} alt="UserImg"/>
         </div>
         <div className="col-11 text-center datosUser">
-          <p className="datosNombre">Aquí va nombre</p>
-          <p className="datosMail">Aquí va mail</p>
+          <p className="datosNombre">{user.displayName}</p>
+          <p className="datosMail">{user.email}</p>
         </div>
       </div>
     )
