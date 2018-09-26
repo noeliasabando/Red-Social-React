4a14e1fff9044df58dd9e02fa171d6613b10599c
Sabando Gonzalez <lolia_15@hotmail.com>
2018-09-26 19:37:08 -0400

    like en perfil mas diseño

--- src/components/PublicacionPerfil/PublicacionPerfil.jsx
+++ src/components/PublicacionPerfil/PublicacionPerfil.jsx
@@ -1,20 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './PublicacionPerfil.css';
 /* import publicacionImg from "../../img/usuaria2.png" */
 
 
-class PublicacionPerfil extends Component{
-  render(){
-    return(
+class PublicacionPerfil extends Component {
+  render() {
+    return (
       <div className="contenedorPublicacion col-4 offset-4">
         <div className="col-12 text-center">
-          <p className="userName">{this.props.author}</p> 
-        </div> 
+          <p className="userName">{this.props.author}</p>
+        </div>
         <div className="">
           <p className="publicacion">{this.props.text}</p>
         </div>
         <div className="">
-          <p className="likes">{this.props.likes}</p>
+          <p className="likes"><FontAwesomeIcon icon="heart" className="corazon"/>{this.props.likes}</p>
         </div>
       </div>
     )
